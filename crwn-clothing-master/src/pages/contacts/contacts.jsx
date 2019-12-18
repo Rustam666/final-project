0a1d5656a9894ea5ff1a772eb1437f8b166b6481
islamicelam <56446607+islamicelam@users.noreply.github.com>
2019-12-18 18:09:32 +0400

    final-project

---
+++ crwn-clothing-master/src/pages/contacts/contacts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../components/bootstrap/bootstrap-grid.css'
+import '../../components/bootstrap/style.css'
+import  '../../components/bootstrap/bootstrap-reboot.css'
+import '../../components/main.scss'
+import Contact from "../../components/contact-section/contact";
+
+const Contacts = () => (
+    <div>
+        <Contact/>
+    </div>
+);
+
+export default Contacts
